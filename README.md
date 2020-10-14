599e4a11b6630b2abaabf7fdd5e998f629524a70
Jacob Swanson <me@jacob-swanson.com>
2020-10-13 20:24:14 -0500

    Clear out the old site

--- README.md
+++
@@ -1,2 +0,0 @@
-# personal-site
-https://www.jacob-swanson.com
--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import {hydrate, render} from "react-dom";
-import registerServiceWorker from "./registerServiceWorker";
-import {App} from "./App";
-
-const rootElement = document.getElementById("root");
-const renderMethod = rootElement && rootElement.hasChildNodes() ? hydrate : render;
-renderMethod(<App/>, rootElement);
-registerServiceWorker();
