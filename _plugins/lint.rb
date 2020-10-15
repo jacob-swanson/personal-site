d2be4c25dafde6599b33bf7db36106e73fecd70b
Jacob Swanson <me@jacob-swanson.com>
2020-10-14 23:03:52 -0500

    Check external links

--- _plugins/lint.rb
+++ _plugins/lint.rb
@@ -4,7 +4,8 @@ Jekyll::Hooks.register :site, :post_write do |site|
   HTMLProofer.check_directory(site.config["destination"], opts = {
     :check_html => true,
     :check_img_http => true,
-    :disable_external => true,
+    :disable_external => false,
+    :only_4xx => true,
     :report_invalid_tags => true,
   }).run
 end
