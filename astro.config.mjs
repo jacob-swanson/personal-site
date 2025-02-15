d053b962335ab47d3c8c23aacafa99452684416b
Jacob Swanson <me@jacob-swanson.com>
2025-02-15 16:59:54 -0600

    Add .html file redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -39,5 +39,16 @@ export default defineConfig({
         "/tech/2019/02/16/capturing-game-audio-separately-using-shadowplay-and-voicemeeter-banana": "/posts/capturing-game-audio-separately-using-shadowplay-and-voicemeeter-banana",
         "/tech/2018/12/18/installing-openjdk11-on-ubuntu-1804": "/posts/installing-openjdk11-on-ubuntu-1804",
         "/tech/2018/11/26/printer-pid-tuning": "/posts/printer-pid-tuning",
+        "/tech/2023/09/19/nixos-proxmox-lxc-setup.html": "/posts/nixos-proxmox-lxc-setup",
+        "/tech/2020/09/24/making-a-btrfs-swapfile.html": "/posts/making-a-btrfs-swapfile",
+        "/tech/2020/07/21/bootstraping-ubuntu.html": "/posts/bootstraping-ubuntu",
+        "/tech/2020/06/21/windows-fresh-install.html": "/posts/windows-fresh-install",
+        "/tech/2020/06/21/fix-broken-steam-desktop-icons.html": "/posts/fix-broken-steam-desktop-icons",
+        "/tech/2019/11/08/firefox-containers-with-aws.html": "/posts/firefox-containers-with-aws",
+        "/tech/2019/05/15/change-docker-ip-range.html": "/posts/change-docker-ip-range",
+        "/tech/2019/02/24/flashing-leneageos-14-to-nexus-7-2012.html": "/posts/flashing-lineageos-14-to-nexus-7-2012",
+        "/tech/2019/02/16/capturing-game-audio-separately-using-shadowplay-and-voicemeeter-banana.html": "/posts/capturing-game-audio-separately-using-shadowplay-and-voicemeeter-banana",
+        "/tech/2018/12/18/installing-openjdk11-on-ubuntu-1804.html": "/posts/installing-openjdk11-on-ubuntu-1804",
+        "/tech/2018/11/26/printer-pid-tuning.html": "/posts/printer-pid-tuning",
     }
 });
